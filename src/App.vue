7ff00ad519e5df324702e8aef04aad767cf114b6
Beatriz Campanha <bcampanha@usp.br>
2024-05-07 17:16:33 -0300

    Arrumando barra de navegação e formatação

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <ul class="navegacao">
-    <li><button @click="pagina=0">Sobre</button></li>
-    <li><button @click="pagina=1">Simulador</button></li>
+    <li><button @click="pagina=0" :class="{'ativo' : pagina===0 }">Sobre</button></li>
+    <li><button @click="pagina=1" :class="{'ativo' : pagina===1 }">Simulador</button></li>
   </ul>
   <img class="logo" alt="Fita dupla de DNA" src="./assets/dna.png">
   <SobreTrabalho v-if="pagina===0"/>
@@ -33,6 +33,21 @@ export default {
 li {
   display: inline-block;
 }
+.ativo {
+  background-color: mediumseagreen;
+}
+button {
+  padding: 0.5rem 1rem;
+  background-color: #333;
+  color: white;
+  border-style: none;
+  border-top-left-radius: 1rem;
+  border-top-right-radius: 1rem;
+  border-bottom: none;
+}
+button:hover{
+  background-color: black;
+}
 </style>
 
 <style>
@@ -49,14 +64,6 @@ li {
 a {
   font-weight: 600;
   color: mediumseagreen;
-}
-button {
-  padding: 0.5rem 1rem;
-  background-color: lightgray;
-  border-style: none
-}
-button:hover, button:focus {
-  background-color: mediumseagreen;
 }
 ul {
   list-style-type: none;
