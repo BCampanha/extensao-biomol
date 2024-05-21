4e0b1530be56708fab92c22fe39c8d0e5fd6cbdb
Beatriz Campanha <bcampanha@usp.br>
2024-05-21 14:03:17 -0300

    Ideia 0

--- src/views/QuestaoDois.vue
+++ src/views/QuestaoDois.vue
@@ -1,7 +1,23 @@
 <template>
   <div>
     <JanelaQuestao>
+      <template #orientacoes>
+        Complete o vetor de transformação do plasmídeo Ti
+      </template>
+      <template #principal>
+        <hr />
+        <div class="bloco-vetor" v-for="bloco in blocos" :key="bloco.nome">
+          {{ bloco.nomebloco }}
+        </div>
+      </template>
       <template #ferramentas>
+        <button
+          v-for="opcao in opcoes"
+          :key="opcao.nome"
+          @click="selecionado(opcao)"
+        >
+          {{ opcao.nome }}
+        </button>
       </template>
     </JanelaQuestao>
   </div>
@@ -15,7 +31,29 @@ export default {
   components: {
     JanelaQuestao,
   },
+  data() {
+    return {
+      blocos: [],
+      opcoes: [
+        {
+          nome: "Origem de replicação agrobacterium",
+          nomebloco: "Ori Agro",
+        },
+      ],
+    };
+  },
+  methods: {
+    selecionado(opcao) {
+      this.blocos.push(opcao);
+    },
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.bloco-vetor {
+  max-width: fit-content;
+  border: 1px solid mediumorchid;
+  padding: 0.5rem;
+}
+</style>
