df5ef7ca74e3a2442c2f3ebaff91e12648fd8294
Kawan Santana <kawansantana18@gmail.com>
2024-06-27 15:54:21 -0300

    Add imagem estática para transformção nuclear

--- src/views/TransformacaoNuclear.vue
+++ src/views/TransformacaoNuclear.vue
@@ -2,7 +2,10 @@
   <div>
     <JanelaQuestao titulo="Transformação Nuclear" orientacoes="A célula hospedeira integra o T-DNA ao seu genoma utilizando sua própria maquinaria.">
       <template #principal>
-        <span></span>
+        <img src="../assets/transformacao-nuclear.jpg" alt="Transformação nuclear." class="mx-auto">
+        <div class="flex justify-center py-2">
+          <small>https://www.sigmaaldrich.com/BR/pt/products/molecular-biology-and-functional-genomics/cloning-and-expression/molecular-cloning-and-protein-expression</small>
+        </div>
       </template>
       <template #ferramentas>
         <span></span>
